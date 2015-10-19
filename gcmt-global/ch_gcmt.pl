7ef077b767c8cfd1b8f8687a0617442bf44fc619
junlysky <junlysky.info@gmail.com>
2015-10-19 11:12:08 +0800

    转换GCMT中ndk格式文件，提供1976-2015年gcmt公布的所有震源机制解结果

--- gcmt-global/ch_gcmt.pl
+++ gcmt-global/ch_gcmt.pl
@@ -3,14 +3,14 @@ use warnings;
 use strict;
 
 my $usage="perl $0 lon_min lon_max lat_min lat_max";
-@ARGV >= 4 or die "$usage \n ";
+@ARGV >= 4 or die "Usage:$usage \n ";
 our $lon_min = $ARGV[0];
 our $lon_max = $ARGV[1];
 our $lat_min = $ARGV[2];
 our $lat_max = $ARGV[3];
 
-open(IN,"< global.mec") or die "open global_gcmt.dat failed ";
-open(OUT,"> plout.dat") or die "can not build out.dat";
+open(IN,"< jan1976_apr2015.dat") or die "open global_gcmt.dat failed ";
+open(OUT,"> mec.out") or die "can not build out.dat";
 while(<IN>){
     our ($date,$time,$lon,$lat,@other)= split /\s+/;
     if( $lon >= $lon_min && $lon <= $lon_max && $lat >= $lat_min && $lat <= $lat_max){
---
+++ src/ch_gcmt.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $usage="perl $0 lon_min lon_max lat_min lat_max";
+@ARGV >= 4 or die "$usage \n ";
+our $lon_min = $ARGV[0];
+our $lon_max = $ARGV[1];
+our $lat_min = $ARGV[2];
+our $lat_max = $ARGV[3];
+
+open(IN,"< global.mec") or die "open global_gcmt.dat failed ";
+open(OUT,"> plout.dat") or die "can not build out.dat";
+while(<IN>){
+    our ($date,$time,$lon,$lat,@other)= split /\s+/;
+    if( $lon >= $lon_min && $lon <= $lon_max && $lat >= $lat_min && $lat <= $lat_max){
+        print OUT "$date $time $lon $lat @other \n";
+    }
+}
+close(OUT);
+close(IN);
